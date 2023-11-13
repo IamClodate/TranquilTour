aebdfec8801d682ac1972c4e8c0a1eeb01583a61
IamClodate <clodatemnisi@gmail.com>
2023-11-13 13:40:20 +0200

    fixed the package.json so that they can run throughh the browser when I user

--- src/api/logs.js
+++ src/api/logs.js
@@ -1,26 +1,26 @@
-const { Router } require ('express');
+// const { Router } require ('express');
 
-const LogEntry = require('../models/LogEntry')
+// const LogEntry = require('../models/LogEntry')
 
-const router = Router();
+// const router = Router();
 
-router.get('/',  async (req, res) => {
-    const entries = await LogEntry.find();
-    res.json(entries);
-});
+// router.get('/',  async (req, res) => {
+//     const entries = await LogEntry.find();
+//     res.json(entries);
+// });
 
-router.post('/', async (req, res, next) => {
-    try {
-        const logEntry = new LogEntry(req.body);
-        const createdEntry = await logEntry.save();
-        res.json(createdEntry);
-    } catch (error) {
-        console.log(error.name);
-        if (error.name === 'ValidationError') {
-            res.status(422);
-        }
-        next(error);
-    }
-});
+// router.post('/', async (req, res, next) => {
+//     try {
+//         const logEntry = new LogEntry(req.body);
+//         const createdEntry = await logEntry.save();
+//         res.json(createdEntry);
+//     } catch (error) {
+//         console.log(error.name);
+//         if (error.name === 'ValidationError') {
+//             res.status(422);
+//         }
+//         next(error);
+//     }
+// });
 
-module.exports = router;
+// module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,64 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+const rootElement = document.getElementById("HAVE TO WRITE here")
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-
-
-//My codings 
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-require('dotenv').config();
-
-const middlewares = require('./middlewares');
-const logs = require('./api/logs');
-
-const app = express();
-
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-
-app.use(morgan('common'));
-app.use(helmet());
-app.use(cors({
-    origin: 'process.env.CORS_ORIGIN',
-}));
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello Word',
-    });
-});
-
-
-app.use(middlewares.notFound);
-
-app .use(middlewares.errorHandler);
-
-const port = process.env.PORT || 1337;
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
-});
-
+    <StrictMode>
+        <App /> 
+    </StrictMode>
+)
